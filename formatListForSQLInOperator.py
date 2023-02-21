391cb046a83c3dc268c24315dd6462551f75af94
Deanna Cook <deannarcook@outlook.com>
2023-02-21 13:39:39 -0500

    Update formatListForSQLInOperator.py
    Updated comments.

--- formatListForSQLInOperator.py
+++ formatListForSQLInOperator.py
@@ -1,7 +1,21 @@
-# provide list of values to format
-lst = ""
+
+# The purpose of this script is to format a list of values for use in the SQL In operator.
+# Prior to executing the script, do the following:
+# 1. Update the lst variable so that it contains the list of values that should be formatted for the SQL In operator Values should be provided in the lst variable using the following format:
+    # """value1
+    # value2
+    # value3 """
+# 2. Update the chrInLine variable indicating how many characters are in each item in lst. For the example above (value1, value2, value 3) chrInLine should be set to 6 because there are 6 characters in each item.
+
+# Development next steps: Update script so that user is prompted to enter values for lst and chrInLine instead of having to hard code it into the script.
+
+# ---> UPDATE VARIABLE BELOW BEFORE RUNNING <---
+lst = """value1
+value2
+value3 """
 
 def formatListForSQLInOperator(lst):
+# ---> UPDATE VARIABLE BELOW BEFORE EXECUTING <---
     chrInLine = 6
     lstAsString = ''.join(lst)
     x = 0
